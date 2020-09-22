bf61d73c3a4f22048d950c64b557a3c89ab27e17
bobo mallick <mallickbobo@gmail.com>
2020-09-22 18:40:42 +0530

    done

---
+++ src/Components/Hotel/Hotel.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Hotel = () => {
+    return (
+        <div>
+          <h1>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellat corrupti quas minus eveniet officia odit ullam provident enim, debitis dolorem accusamus dignissimos reprehenderit velit itaque illo. Porro est, dolor, at quasi, repudiandae tempora quaerat voluptatum cumque blanditiis perspiciatis facilis aliquam!</h1>  
+          <h1>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellat corrupti quas minus eveniet officia odit ullam provident enim, debitis dolorem accusamus dignissimos reprehenderit velit itaque illo. Porro est, dolor, at quasi, repudiandae tempora quaerat voluptatum cumque blanditiis perspiciatis facilis aliquam!</h1>  
+        </div>
+    );
+};
+
+export default Hotel;
